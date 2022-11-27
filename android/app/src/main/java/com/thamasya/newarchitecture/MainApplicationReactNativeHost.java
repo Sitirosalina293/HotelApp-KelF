52c35a49ed95636f3d38ac7ec3da2c327bf65129
Siti Rosalina <sitirosalina29@gmail.com>
2022-11-27 11:26:08 +0700

    ganti nama & icon

--- android/app/src/main/java/com/thamasya/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/thamasya/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.thamasya.newarchitecture;
+package com.hotelapp.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.thamasya.BuildConfig;
-import com.thamasya.newarchitecture.components.MainComponentsRegistry;
-import com.thamasya.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.hotelapp.BuildConfig;
+import com.hotelapp.newarchitecture.components.MainComponentsRegistry;
+import com.hotelapp.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- android/app/src/main/java/com/thamasya/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/thamasya/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.thamasya.newarchitecture.components;
+package com.hotelapp.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
